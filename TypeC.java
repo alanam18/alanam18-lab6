b6d41fc60112b2e459dbfc73e7d19d6ba5b2c0a9
alanam18 <alanam1@gwmail.gwu.edu>
2026-03-18 01:26:23 -0400

    Added TypeC with diagonal zigzag movement

--- TypeC.java
+++ TypeC.java
@@ -1,1 +1,18 @@
+public class TypeC extends Thing {
+    private boolean zigLeft = true;
 
+    public TypeC() {
+        row = 50;
+        col = 60;
+        lab = 'y';
+    }
+
+    public void maybeTurn() {
+        if (zigLeft) {
+            leftTurn();
+        } else {
+            rightTurn();
+        }
+        zigLeft = !zigLeft;
+    }
+}
