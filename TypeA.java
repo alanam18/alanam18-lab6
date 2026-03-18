b213d9033e0547c79819e6972e386ab0bc169481
alanam18 <alanam1@gwmail.gwu.edu>
2026-03-17 23:39:09 -0400

    Added TypeA class with polymorphic maybeTurn

--- TypeA.java
+++ TypeA.java
@@ -1,1 +1,12 @@
-
+public class TypeA extends Thing {
+    public TypeA() {
+        row = 45;
+        col = 50;
+        lab = 'r';
+    }
+    public void maybeTurn() {
+        int a = rand.nextInt(3);
+        if (a == 1) rightTurn();
+        if (a == 2) leftTurn();
+    }
+}
