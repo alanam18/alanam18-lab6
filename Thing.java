52c690864aba911cdcb7da7bc6f0298b1b92f709
alanam18 <alanam1@gwmail.gwu.edu>
2026-03-17 22:20:03 -0400

    Added the abstract parent class Thing with its shared fields and methods

--- Thing.java
+++ Thing.java
@@ -1,1 +1,25 @@
+public abstract class Thing {
+    public int row, col, direct, timeSinceLast;
+    public char lab = 'r';
+    public static final int[] dc = {0, 1, 0, -1};
+    public static final int[] dr = {1, 0, -1, 0};
 
+    public void rightTurn() {
+        direct = (direct + 1) % 4;
+    }
+
+    public void leftTurn() {
+        direct = (direct + 3) % 4;
+    }
+
+    public void step() {
+        row += dr[direct];
+        col += dc[direct];
+    }
+
+    public abstract void maybeTurn();
+
+    public String toString() {
+        return row + " " + col + " " + lab;
+    }
+}
