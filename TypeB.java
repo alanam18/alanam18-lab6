e262ae454a483b235efaffff1613dd6b89708009
alanam18 <alanam1@gwmail.gwu.edu>
2026-03-18 01:02:13 -0400

    Added TypeB and its maybeTurn similar to TypeA

--- TypeB.java
+++ TypeB.java
@@ -1,1 +1,18 @@
+public class TypeB extends Thing {
+    public TypeB() {
+        row = 55;
+        col = 50;
+        lab = 'b';
+    }
 
+   
+    public void maybeTurn() {
+        int i = rand.nextInt(3);
+        timeSinceLast++;
+        if (timeSinceLast == 10) {
+            timeSinceLast = 0;
+            if (i == 1) rightTurn();
+            if (i == 2) leftTurn();
+        }
+    }
+}
