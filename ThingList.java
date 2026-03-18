78028970b5067007f4e4e3ea73514af5bac40a4e
alanam18 <alanam1@gwmail.gwu.edu>
2026-03-18 01:24:39 -0400

    Added ThingList with private Node inner class and add,printAll, and moveAll methods

--- ThingList.java
+++ ThingList.java
@@ -1,1 +1,27 @@
+public class ThingList {
+    private class Node {
+        Thing data;
+        Node next;
+    }
 
+    private Node head;
+
+    public void add(Thing t) {
+        Node n = new Node();
+        n.data = t;
+        n.next = head;
+        head = n;
+    }
+
+    public void printAll() {
+        for (Node T = head; T != null; T = T.next)
+            System.out.println(T.data.toString());
+    }
+
+    public void moveAll() {
+        for (Node T = head; T != null; T = T.next) {
+            T.data.maybeTurn();
+            T.data.step();
+        }
+    }
+}
